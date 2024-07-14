9716ea0c20303d1df5f3729dd8b66d1fae3b4b4a
CNeilas <humanmusics@gmail.com>
2024-07-14 19:19:12 +0200

    aaaaHHHHHHHH

--- script.js
+++ script.js
@@ -1,61 +1,93 @@
-// 1. take the values out of html
-// 2. Populate the display with numbers and an operator
-// 3. if there is an operator already populate the second number
-// 4. make functions for all the operators
-// 5. take out the display thing and according to the operator make an operation
-
 // taking display
-let display = document.getElementsByClassName("display")
-let firstNum = document.getElementById("one")
-let secondNum = document.getElementById("two")
-let operator = document.getElementById("operator")
+let display = document.querySelector(".display");
+let firstNum = document.getElementById("one");
+let secondNum = document.getElementById("two");
+let operator = document.getElementById("operator");
 
 // taking buttons and ops wagwan
-let numbers = document.getElementsByClassName("number")
-let operators = document.getElementsByClassName("op")
-let equals = document.getElementsByClassName("equals")
-let clear = document.getElementById("C")
+let numbers = document.getElementsByClassName("number");
+let operators = document.getElementsByClassName("op");
+let equals = document.querySelector(".equals");
+let clear = document.querySelector(".C");
 
 // operator and firstNum is not selected and not equals yet
-let operationHappening = false
-let isFirstNumber = false
-let isEqual = true
+let operationHappening = false;
+let isFirstNumber = false;
+let isSecondNumber = false;
+let isEqual = false;
 
 // function for populating display with numbers
-const handleNumbers = e => {
-    let value = e.target.innerHTML
+const handleNumbers = (e) => {
+  let value = e.target.innerHTML;
 
-    if (operationHappening) {
-        if (secondNum.innerHTML == "_" ) {
-            secondNum.innerHTML = ""
-        }
-        secondNum.innerHTML += value
-    } else if (!operationHappening) {
-        if (firstNum.innerHTML == "_" ) {
-            firstNum.innerHTML = ""
-        }
-        firstNum.innerHTML += value
-        // firstNum is now selected
-        isFirstNumber = true
+  if (operationHappening) {
+    if (secondNum.innerHTML == "_") {
+      secondNum.innerHTML = "";
     }
-}
+    secondNum.innerHTML += value;
+    isSecondNumber = true;
+  } else if (!operationHappening) {
+    if (firstNum.innerHTML == "_") {
+      firstNum.innerHTML = "";
+    }
+    firstNum.innerHTML += value;
+    // firstNum is now selected
+    isFirstNumber = true;
+  }
+};
 
 for (let number of numbers) {
-    number.addEventListener("click", handleNumbers)
+  number.addEventListener("click", handleNumbers);
 }
 
 // function for selecting operator
 
-const handleOperators = e => {
-    let op = e.target.innerHTML
-    if (isFirstNumber) {
-        operator.innerHTML = op
-        operationHappening = true
-    } else if (!isFirstNumber) {
-        alert("select a number first")
+const handleOperators = (e) => {
+
+  let op = e.target.innerHTML;
+      if (isEqual) {
+        let firstNum = display.innerHTML
+        display.innerHTML += operator
+        
     }
-}
+  operator.innerHTML = op;
+  operationHappening = true;
+};
 
 for (let opButton of operators) {
-    opButton.addEventListener("click", handleOperators)
+  opButton.addEventListener("click", handleOperators);
 }
+
+// wtf do i do for the equals XDDDD
+
+const handleEquals = () => {
+  display.innerHTML = calculate(
+    operator.innerHTML,
+    firstNum.innerHTML,
+    secondNum.innerHTML
+  );
+  isEqual = true;
+
+  console.log(
+    calculate(operator.innerHTML, firstNum.innerHTML, secondNum.innerHTML)
+  );
+};
+
+const calculate = (op, num1, num2) => {
+  switch (op) {
+    case "+":
+      return Number(num1) + Number(num2);
+      break;
+    case "-":
+      return Number(num1) - Number(num2);
+      break;
+    case "/":
+      return Number(num1) / Number(num2);
+      break;
+    case "*":
+      return Number(num1) * Number(num2);
+      break;
+  }
+};
+
+equals.addEventListener("click", handleEquals);
