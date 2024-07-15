a9eb53a2a497b82ad26b44d2a494a5cb7853eeb5
nsunklodas <farmeris108@gmail.com>
2024-07-15 18:24:14 +0300

    finished while sober and made phone size media query

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-// taking display
+// Selecting display elements
 let display = document.querySelector(".display");
 let firstNum = document.getElementById("one");
 let secondNum = document.getElementById("two");
 let operator = document.getElementById("operator");
 
-// taking buttons and ops wagwan
+// Selecting buttons and operators
 let numbers = document.getElementsByClassName("number");
 let operators = document.getElementsByClassName("op");
 let equals = document.querySelector(".equals");
 let clear = document.querySelector(".C");
 
-// operator and firstNum is not selected and not equals yet
+// State variables
 let operationHappening = false;
-let isFirstNumber = false;
 let isSecondNumber = false;
-let isEqual = false;
+let resultDisplayed = false;
 
-// function for populating display with numbers
+// Function for populating display with numbers
 const handleNumbers = (e) => {
   let value = e.target.innerHTML;
 
-  if (operationHappening) {
-    if (secondNum.innerHTML == "_") {
-      secondNum.innerHTML = "";
-    }
+  if (resultDisplayed) {
+    firstNum.innerHTML = value;
+    secondNum.innerHTML = '';
+    operator.innerHTML = '';
+    operationHappening = false;
+    isSecondNumber = false;
+    resultDisplayed = false;
+  } else if (operationHappening) {
     secondNum.innerHTML += value;
     isSecondNumber = true;
-  } else if (!operationHappening) {
-    if (firstNum.innerHTML == "_") {
-      firstNum.innerHTML = "";
-    }
+  } else {
     firstNum.innerHTML += value;
-    // firstNum is now selected
-    isFirstNumber = true;
   }
 };
 
@@ -40,54 +38,89 @@ for (let number of numbers) {
   number.addEventListener("click", handleNumbers);
 }
 
-// function for selecting operator
-
+// Function for selecting operator
 const handleOperators = (e) => {
-
   let op = e.target.innerHTML;
-      if (isEqual) {
-        let firstNum = display.innerHTML
-        display.innerHTML += operator
-        
-    }
-  operator.innerHTML = op;
-  operationHappening = true;
+
+  if (isSecondNumber) {
+    firstNum.innerHTML = calculate(
+      operator.innerHTML,
+      firstNum.innerHTML,
+      secondNum.innerHTML
+    );
+    secondNum.innerHTML = '';
+    operator.innerHTML = op;
+    isSecondNumber = false;
+    operationHappening = true;
+    resultDisplayed = false;
+  } else if (firstNum.innerHTML !== '') {
+    operator.innerHTML = op;
+    operationHappening = true;
+    resultDisplayed = false;  // Allow chaining after equals
+  }
 };
 
 for (let opButton of operators) {
   opButton.addEventListener("click", handleOperators);
 }
 
-// wtf do i do for the equals XDDDD
-
+// Function for handling equals
 const handleEquals = () => {
-  display.innerHTML = calculate(
-    operator.innerHTML,
-    firstNum.innerHTML,
-    secondNum.innerHTML
-  );
-  isEqual = true;
-
-  console.log(
-    calculate(operator.innerHTML, firstNum.innerHTML, secondNum.innerHTML)
-  );
-};
-
-const calculate = (op, num1, num2) => {
-  switch (op) {
-    case "+":
-      return Number(num1) + Number(num2);
-      break;
-    case "-":
-      return Number(num1) - Number(num2);
-      break;
-    case "/":
-      return Number(num1) / Number(num2);
-      break;
-    case "*":
-      return Number(num1) * Number(num2);
-      break;
+  if (firstNum.innerHTML !== '' && secondNum.innerHTML !== '' && operator.innerHTML !== '') {
+    firstNum.innerHTML = calculate(
+      operator.innerHTML,
+      firstNum.innerHTML,
+      secondNum.innerHTML
+    );
+    operator.innerHTML = '';
+    secondNum.innerHTML = '';
+    resultDisplayed = true;
+    operationHappening = false;
+    isSecondNumber = false;
   }
 };
 
 equals.addEventListener("click", handleEquals);
+
+// Function for handling clear
+const handleClear = () => {
+  firstNum.innerHTML = '';
+  secondNum.innerHTML = '';
+  operator.innerHTML = '';
+  operationHappening = false;
+  isSecondNumber = false;
+  resultDisplayed = false;
+};
+
+clear.addEventListener("click", handleClear);
+
+// Calculation function
+const calculate = (op, num1, num2) => {
+  let result;
+  switch (op) {
+    case '+':
+      result = Number(num1) + Number(num2);
+      break;
+    case '-':
+      result = Number(num1) - Number(num2);
+      break;
+    case '/':
+      result = Number(num1) / Number(num2);
+      break;
+    case '*':
+      result = Number(num1) * Number(num2);
+      break;
+    default:
+      result = 'Error';
+  }
+
+  return formatResult(result);
+};
+
+// Function for formatting result
+const formatResult = (result) => {
+  if (result % 1 !== 0) {
+    return result.toFixed(3);
+  }
+  return result;
+};
